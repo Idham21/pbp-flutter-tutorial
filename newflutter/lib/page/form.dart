966df9b0c7b754be68c7c0b05987f3b6fb76f63e
Idham.v21 <idhamv13@gmail.com>
2022-11-24 11:13:13 +0700

    fixed lab 8

--- newflutter/lib/page/form.dart
+++ newflutter/lib/page/form.dart
@@ -1,6 +1,8 @@
 import 'package:newflutter/main.dart';
 import 'package:flutter/material.dart';
 
+import 'to_do_page.dart';
+
 class MyFormPage extends StatefulWidget {
   const MyFormPage({super.key});
 
@@ -47,6 +49,16 @@ class _MyFormPageState extends State<MyFormPage> {
                 Navigator.pushReplacement(
                   context,
                   MaterialPageRoute(builder: (context) => const MyFormPage()),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('To Do'),
+              onTap: () {
+                // Route menu ke halaman to do
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ToDoPage()),
                 );
               },
             ),
